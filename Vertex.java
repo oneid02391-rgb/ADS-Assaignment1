6b4aed7d9633ac209198d4153190cfa72c51e8a4
oneid02391-rgb <one.id02391@icloud.com>
2026-05-19 19:04:28 +0500

    Update and rename MyStack.java to Vertex.java

---
+++ Vertex.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex<V> {
+
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+
+    public Vertex(V data) {
+        this.data = data;
+        adjacentVertices = new HashMap<>();
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
